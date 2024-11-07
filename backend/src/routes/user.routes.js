481991e33c62703f943e44662308f6ee77f0424f
Chetanrathore-13 <chetanrathore9131@gmail.com>
2024-11-07 10:48:56 +0530

    added controller

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,7 +1,13 @@
 import { Router } from "express";
 import passport from "passport";
+import { userRegisterValidator } from "../validators/user.validator.js";
+import { validate } from "../validators/validate.js";
+
 
 const router = Router();
 
+// unsecure routes
+router.route("/register").post(userRegisterValidator(), validate, registerUser);
+router.route("/login").post(userLoginValidator(), validate, loginUser);
 
 export default router 
